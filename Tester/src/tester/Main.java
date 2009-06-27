b9103c5ec73aad07e298a51b7b219b3558180c7e
richard_hawkes <richard_hawkes@yahoo.co.uk>
2009-06-27 12:17:55 +0000

    Changed hello message

--- Tester/src/tester/Main.java
+++ Tester/src/tester/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Aha!");
+        System.out.println("Take on me!");
     }
 
 }
