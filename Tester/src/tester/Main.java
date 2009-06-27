33c4e4eeedebd34900d615d90fa2bb384e5ba083
richard_hawkes <richard_hawkes@yahoo.co.uk>
2009-06-27 12:29:31 +0000

    Another aha song

--- Tester/src/tester/Main.java
+++ Tester/src/tester/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Take on me!");
+        System.out.println("The sun always shines on TV!");
     }
 
 }
