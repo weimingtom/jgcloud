03b7357926765c2170be38a837995b75eff84302
richard_hawkes <richard_hawkes@yahoo.co.uk>
2009-11-24 23:19:59 +0000

    Updated to set the JAVA path, and also added a stop batch file :)

--- TestDarkstarServer/conf/startDarkstarTestServer.bat
+++ TestDarkstarServer/conf/startDarkstarTestServer.bat
@@ -1,1 +1,4 @@
+set JAVA_HOME="c:\Program Files (x86)\Java\jdk1.6.0_18"
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 java -jar sgs-server-dist-0.9.11\bin\sgs-boot.jar sgs-server-dist-0.9.11\tutorial\conf\TestDarkstarServer.boot
